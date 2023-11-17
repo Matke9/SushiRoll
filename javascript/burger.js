c3844927dd302360d5d7f95e887d006b45f49e06
Matke9 <matovicmihailo@gmail.com>
2023-11-18 00:37:22 +0100

    DOSTA BRE VISEU

--- javascript/burger.js
+++ javascript/burger.js
@@ -1,8 +1,31 @@
+const otvoreno = false;
+
 function OpenZaklopen() {
     var x = document.getElementById("navigation");
-    if (x.style.display === "flex") {
-        x.style.display = "none";
+
+    if (x.style.height === "150px") {
+        x.style.height = "0";
     } else {
-        x.style.display = "flex";
+        x.style.height = "150px";
     }
 }
+
+
+function setNavigationHeight() {
+    var x = document.getElementById("navigation");
+
+    const windowWidth = window.innerWidth;
+    if (windowWidth > 700) {
+        x.style.height = '40px';
+    } else if (x.style.height === '150px') {
+        x.style.height = '150px';
+    } else {
+        x.style.height = '0';
+    }
+}
+
+// Detect the resize event of the window and call the function
+window.addEventListener('resize', setNavigationHeight);
+
+// Call the function initially to set the correct height
+setNavigationHeight();
